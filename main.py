3f4039cf907e141fd3b94f82045a408238eb2336
dimondimskiy <skdmyr@gmail.com>
2023-05-07 08:46:35 +0200

    working

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
-from telegram import Update
+import os
+import subprocess
+
+
+from yt_dlp import YoutubeDL, utils
+from telegram import Update, error
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, filters
 from telegram.constants import MessageEntityType
+
+
+TEMP_NAME = 'tmp.mp4'
+EXPECTED_FORMAT = '.mp4'
+MAX_FILE_SIZE = 52428800
 
 
 async def bot_text(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
@@ -13,7 +23,47 @@ async def bot_answer(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None
         await update.message.reply_text(f"{update.effective_user.first_name}, you entered to much URLs")
     else:
         url = urls.popitem()[1]
-        await update.message.reply_text(f"{url}")
+
+        await update.message.reply_text("Downloading...")
+        try:
+            download(url)
+            await update.message.reply_text("File downloaded and converted")
+        except utils.DownloadError:
+            await update.message.reply_text(f"{update.effective_user.first_name}, you entered an unsupported URL")
+            return
+
+        filesize = os.path.getsize(TEMP_NAME)
+        if filesize > MAX_FILE_SIZE:
+            await update.message.reply_text("To large file to upload")
+            return
+
+        await update.message.reply_text("Uploading...")
+        try:
+            await update.message.reply_document(TEMP_NAME, write_timeout=240)
+        except error.NetworkError:
+            await update.message.reply_text("Uploading failed, network error")
+
+
+def ensure_mp4(filename: str) -> None:
+    _, ext = os.path.splitext(filename)
+    if ext != EXPECTED_FORMAT:
+        subprocess.run(['ffmpeg', '-i', filename, TEMP_NAME, '-y'])
+        os.remove(filename)
+    else:
+        os.rename(filename, TEMP_NAME)
+
+
+params = {
+    'format': 'b[filesize<50M]/ w',
+    'vext': '(mp4 > webm > flv > other > unknown)',
+    'post_hooks': [ensure_mp4],
+    'keep_video': False
+}
+
+
+def download(url: str) -> None:
+    with YoutubeDL(params) as ydl:
+        ydl.download(url)
 
 
 app = ApplicationBuilder().token("6055251020:AAHwkEyhuhsh9-5afrm0tx52cP7KzK9IyBk").build()
@@ -21,26 +71,5 @@ app = ApplicationBuilder().token("6055251020:AAHwkEyhuhsh9-5afrm0tx52cP7KzK9IyBk
 app.add_handler(MessageHandler(filters.Entity(MessageEntityType.URL), bot_answer))
 app.add_handler(MessageHandler(filters.ALL, bot_text))
 
-app.run_polling()
 
-# from telegram import Update
-# from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
-#
-#
-# def hello(update: Update, context: CallbackContext) -> None:
-#     update.message.reply_text(f'Hello {update.effective_user.first_name}')
-#
-#
-# def botMessage(update: Update, context: CallbackContext):
-#     text = update.message.text
-#     reply = bot(text)
-#     update.message.reply_text(reply)
-#
-#
-# updater = Updater('5249226675:AAETSF6NN51dgtMsQuzC4kg79Yjfl_bfny8')
-#
-# updater.dispatcher.add_handler(CommandHandler('hello', hello))
-# updater.dispatcher.add_handler(MessageHandler(Filters.text, botMessage))
-#
-# updater.start_polling()
-# updater.idle()
+app.run_polling()
